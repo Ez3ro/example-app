87f3572606a24d1e9e55ca548d191687a14dc6fc
unknown <Mohammad Michael Mip@JZ28B-239.JZ28B-239>
2023-05-19 13:45:24 +0800

    d-none the class form for connect and disconnecting

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -3,60 +3,43 @@
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="https://js.pusher.com/8.0.1/pusher.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.14/vue.min.js" integrity="sha512-BAMfk70VjqBkBIyo9UTRLl3TBJ3M0c6uyy2VMUrq370bWs7kchLNN9j1WiJQus9JAJVqcriIUX859JOm12LWtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-{{-- <section>
-    <label class="chat-btn" for="check"> 
-        <i class="fa fa-commenting-o comment"></i> 
-        <i class="fa fa-close close"></i> 
-    </label> 
-    <div class="wrapper"> 
-        <div class="header text-white font-bold"> 
-            <p>Live Chat - Online</p>
-        </div> 
-        <div class="p-2"> 
-            <span><b>bMike0123</b></span> 
-        </div> <div class="chat-form"> 
-            <input type="text" class="form-control" placeholder="Name">
-            <textarea class="form-control" placeholder="Your Text Message"></textarea> 
-            <p class="p-0 m-0 ps-2 pe-2" v-for="(message, index) in incomingMessages">
-                (@{{ message.time }}) <b>@{{ message.name }}</b>
-                @{{ message.message }}
-            </p>
-            <button class="btn btn-success btn-block">Submit</button> 
-        </div> 
-    </div>
-</section> --}}
-
 
 <section class="w-25 p-3 col-4">
 <div class="w-full" id="app" >
-    <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+    <a class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
       <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-5 fw-semibold">Live Chat - @{{ state }}</span>
-    </a>
-    <div class="overflow-auto py-3 lh-tight bg-light pt-2 pb-2" style="height:30rem">
+      <span class="fs-5 fw-semibold pr-2">
+        <span v-if="connected == null" class="translate-middle p-2 bg-danger border border-light rounded-circle float-left"> </span>
+        <span v-if="connected == true" class="translate-middle p-2 bg-success border border-light rounded-circle float-left"> </span> 
 
-        <div class="col-12"  v-for="(message, index) in incomingMessages">
+        </span> 
+        Live Chat 
+    </a>
+    <div class="overflow-auto py-3 lh-tight bg-light pt-2 pb-2" style="height:35rem"  >
+        <p class="col-12"  v-for="(message, index) in incomingMessages" id="messageBody">
             (@{{ message.time }}) <b>@{{ message.name }}</b>
             @{{ message.message }}
-        </div>
+        </p>
     </div>
 
     {{-- Input Message --}}
     <div>
-        <form>
+        {{-- hide name and generate code --}}
+    <form class="d-none" >
         <label>Name</label>
         <input class="form-control form-control-sm" placeholder="Name" v-model="name">
-        <button v-if="connected === false" v-on:click="connect()" type="button"
-                class="mr-2 btn btn-sm btn-primary w-100">
-        Connect
+        <button v-if="connected === false" v-on:click="connect()" type="button" class="mr-2 btn btn-sm btn-primary w-100">
+            Connect
         </button>
-        <button v-if="connected === true" v-on:click="disconnect()" type="button"
-                class="mr-2 btn btn-sm btn-danger w-100">
-        Disconnect
+        <button v-if="connected === true" v-on:click="disconnect()" type="button" class="mr-2 btn btn-sm btn-danger w-100">
+            Disconnect
         </button>
     </form>
-      </div>
+
+
+
     <form>
+    </div>
       <div class="row mt-2">
         <div class="col-12 text-white" v-show="formError === true">
           <div class="bg-danger p-2 mb-2">
@@ -65,7 +48,7 @@
         </div>
         <div class="col-12">
           <div class="form-group">
-            <textarea v-model="message" placeholder="Your message ..." class="form-control" rows="3"></textarea>
+            <textarea v-model="message" placeholder="Your message ..." class="form-control" rows="3" id="inpMessage"></textarea>
           </div>
         </div>
       </div>
@@ -100,11 +83,7 @@
       },     
       mounted() {
           this.app = this.apps[0] || null;
-      },
-      methods: {
-          connect() {
-              
-              this.pusher = new Pusher("staging", {
+          this.pusher = new Pusher("staging", {
                   cluster: "mt1",
                   wsHost: "127.0.0.1",
                   wsPort: "6001",
@@ -138,8 +117,10 @@
                   this.formError = true;
               });
 
-              this.subscribeToAllChannels();
-          },
+              this.subscribeToAllChannels();       
+   
+      },
+      methods: {
           subscribeToAllChannels() {
               [
                   "api-message",
@@ -182,7 +163,9 @@
                     alert("Error sending event.")
                 });
             }
-          }
+           this.message = null;
+            // container.scrollBottom = container.scrollHeight;
+          }          
       }
       });
 </script>
